731bb5fa1ce77e4c104c2ddb5ffc010b60a9389a
Erikson Tobing <120314636+EricksonTobing@users.noreply.github.com>
2025-09-08 05:16:52 +0700

    tambah phar

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../inc/config.php';
-require_once __DIR__ . '/../inc/auth.php';
+require_once __DIR__ . '/inc/config.php';
+require_once __DIR__ . '/inc/auth.php';
 if (auth_user()) {
   header('Location: dashboard.php');
 } else {
