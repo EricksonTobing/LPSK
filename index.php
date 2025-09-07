5f61b161941d0601fb0166165b3ab17dc5b5a69c
Erikson Tobing <120314636+EricksonTobing@users.noreply.github.com>
2025-09-08 06:22:26 +0700

    push lagi perbaiki index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/inc/config.php';
 require_once __DIR__ . '/inc/auth.php';
 if (auth_user()) {
-  header('Location: dashboard.php');
+  header('Location: /public/dashboard.php');
 } else {
-  header('Location: login.php');
+  header('Location: /public/login.php');
 }
